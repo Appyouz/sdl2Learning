6d664a5606d288b3b1a3fc8630ccda36310d23ce
appyouz <dikpalkhatri7@gmail.com>
2023-03-26 17:36:46 +0545

    event queue and sdl poll event completed

---
+++ event_queue_and_sdl_poll_event/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[])
+{
+  SDL_Window * window {nullptr};
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "SDL could not be initialized: " << SDL_GetError() << '\n'; 
+  }else {
+    std::cout << "SDL video system is ready to go." << '\n';
+  }
+
+  window = SDL_CreateWindow("first window", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
+
+
+  // game loop event
+   bool gameIsRunning {true};
+
+  while (gameIsRunning)
+  {
+    SDL_Event event;
+    
+    //start event loop
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT) {
+        gameIsRunning = false;
+      } 
+    }
+    
+  }
+
+  SDL_Delay(3000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+
+
+  return 0;
+}
