3e5f18fe1c87079228e44100f460d8d63bc07b7c
appyouz <dikpalkhatri7@gmail.com>
2023-04-16 22:02:06 +0545

    copied

---
+++ 43_timer/include/TextureRectangle.hpp
@@ -0,0 +1,45 @@
+#ifndef TEXTURE_RECTANGLE_H
+#define TEXTURE_RECTANGLE_H
+
+#include <SDL2/SDL.h>
+#include <string>
+
+class TextureRectangle {
+private:
+  SDL_Rect m_rectangle;
+  SDL_Texture *m_texture;
+  inline SDL_Rect GetRectangle() const { return m_rectangle; }
+
+  int m_redColorKey;
+  int m_greenColorKey;
+  int m_blueColorKey;
+
+public:
+  TextureRectangle(SDL_Renderer *renderer, std::string filePath);
+  TextureRectangle(SDL_Renderer *renderer, std::string filePath,
+                   int redColorKey, int greenColorKey, int m_blueColorKey);
+  void initDefaults();
+  ~TextureRectangle();
+
+  SDL_Rect &getSDLRect();
+  SDL_Rect *getSDLRectPtr();
+  SDL_Texture *getSDLTexture();
+
+  void setPosition(int x, int y);
+  void setDimension(int w, int h);
+  int getPositionX();
+  int getPositionY();
+  int getWidth();
+  int getHeight();
+  int GetRedColorKey();
+  int GetGreenColorKey();
+  int GetBlueColorKey();
+
+  void setColorKey(int red, int green, int blue);
+
+  // detect collision
+  void update();
+  void render(SDL_Renderer *renderer);
+};
+
+#endif // !TEXTURE_RECTANGLE_H
